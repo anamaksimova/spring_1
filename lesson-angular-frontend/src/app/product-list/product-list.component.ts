3ae3ae5fffad95bd93311884dbd37431e2a02c48
anamaksimova <19cherrytree91@gmail.com>
2021-07-23 21:27:17 +0600

    10

---
+++ lesson-angular-frontend/src/app/product-list/product-list.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import {ProductService} from "../model/product.service";
+import {Product} from "../model/product";
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.scss']
+})
+export class ProductListComponent implements OnInit {
+  public products: Product[] = [];
+  public isError: boolean = false;
+  constructor(public productService: ProductService) { }
+
+  ngOnInit(): void {
+    this.retrieveProducts();
+  }
+  private retrieveProducts() {
+    this.productService.findAll()
+      .then(res => {
+        this.isError = false;
+        this.products = res;
+      })
+      .catch(err => {
+        this.isError = true;
+        console.log(err);
+      })
+  }
+
+  delete(id: number | null) {
+    if (id == null) {
+      console.error("Calling delete for null id");
+      return;
+    }
+    this.productService.delete(id)
+      .then(() => {
+        this.retrieveProducts();
+      })
+  }
+
+}
